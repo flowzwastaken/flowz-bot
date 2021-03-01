7127d8b2c94e38c737351a1b5e0f24582184a71a
Flowz <79065159+flowzwastaken@users.noreply.github.com>
2021-02-28 21:30:51 -0700

    Delete run.bat

--- run.bat
+++
@@ -1,7 +0,0 @@
-@echo off 
-:: Created by Flowz (https://github.com/flowzwastaken) [2 / 24 / 2021 ] 
-:: -*Read LICENSE to know more about permissions*- 
-title Flowz Ticket System 
-:START 
-node index.js 
-goto START 
